a7d940eb41e8467385978ad69f942276e05dfcce
BigsonLvrocha <luizvictorlrocha@gmail.com>
2019-01-26 14:10:49 -0200

    adds prefabs and initial screen
    adds a preload scene for loading the game manager at the beginning and a loader to be added in each scene

---
+++ Assets/Scripts/Initial/SoundButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundButton : MonoBehaviour
+{
+    private Text buttonText;
+    private GameManager man;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.man = GameManager.Manager;
+        buttonText = this.gameObject.GetComponent<Text>();
+        this.parseButtonText();
+    }
+
+    public void toggleSound()
+    {
+        this.man.SoundOn = !this.man.SoundOn;
+        this.parseButtonText();
+    }
+
+    private void parseButtonText() 
+    {
+        this.buttonText.text = this.man.SoundOn ? "Sound - On" : "Sound - Off";
+    }
+}
