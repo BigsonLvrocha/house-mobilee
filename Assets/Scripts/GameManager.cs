800f6f5db032af79101113c053c18c39518d5f04
BigsonLvrocha <luizvictorlrocha@gmail.com>
2019-01-26 14:04:07 -0200

    adds Management scripts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager manager = null;
+    public static GameManager Manager
+    {
+        get {
+            return GameManager.manager;
+        }
+    }
+
+    public string currentScene = "";
+    bool soundOn = true;
+
+    public bool SoundOn 
+    {
+        get {
+            return this.soundOn;
+        }
+        set {
+            this.soundOn = value;
+        }
+    }
+
+    void Awake() {
+        if (GameManager.manager == null) {
+            GameManager.manager = this;
+        } else if (GameManager.manager != this) {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+        if (this.currentScene == "") {
+            this.LoadScene("Initial");
+        }
+        this.LoadScene(this.currentScene);
+    }
+
+    public void LoadScene(string scene)
+    {
+        this.currentScene = scene;
+        UnityEngine.SceneManagement.SceneManager.LoadScene(currentScene);
+    }
+}
---
+++ Assets/Scripts/Initial/Quit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    public GameObject sure;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void OnQuitClick() 
+    {
+        Application.Quit();
+    }
+
+
+}
