d09a2c9f52aabc83516c25440164be505e8bd1b8
BigsonLvrocha <luizvictorlrocha@gmail.com>
2019-01-26 19:39:05 -0200

    adds scenes skeleton for kitchen, garden and living room

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string destinyScene;
 
     private void  OnMouseDown() 
     {
-        GameManager.Manager.LoadScene("kitchen");
+        GameManager.Manager.LoadScene(this.destinyScene);
     }
 }
