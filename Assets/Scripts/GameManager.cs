8705f7f0730110a63b5b2170aa706a4657b7143c
BigsonLvrocha <luizvictorlrocha@gmail.com>
2019-01-27 15:59:59 -0200

    adds music to initial page

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,7 +18,7 @@ public class GameManager : MonoBehaviour
     /* Properties */
     public Text moneyText;
     public Level level;
-
+    public AudioSource sound;
     [UnityEngine.SerializeField]
     private bool running = false;
     private int score = 0;
@@ -49,6 +49,7 @@ public class GameManager : MonoBehaviour
             return this.soundOn;
         }
         set {
+            this.sound.mute = !value;
             this.soundOn = value;
         }
     }
@@ -66,6 +67,11 @@ public class GameManager : MonoBehaviour
             return;
         }
         this.LoadScene(this.currentScene);
+    }
+
+    public void Start()
+    {
+        this.sound = gameObject.GetComponent<AudioSource>();
     }
 
     public void WinOrLose(){
